2fb7e81d39bdd8e3306dd55bf1429c8cb1d4fa13
Jon Smiley <jasmiley0715@gmail.com>
2019-08-31 12:58:40 -0500

    Set up initial npm packages.

---
+++ pse/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Panel, NavBar, Layout, Main} from 'smiley-design'
+ 
+export default class App extends Component {
+ 
+  render () {
+  // when the navSidebarPosition={"left"} on the NavBar 
+    return (
+      <Main typeContainer="main"> 
+          
+      </Main>
+    )
+  }
+}
